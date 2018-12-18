5cbd387cf103be1b21d21a08bffb69b90cede993
Joon Song <joon7891@gmail.com>
2018-12-18 10:48:33 -0500

    Added Leather Belt class and graphics

---
+++ ISU_Medieval_Odyssey/Item/Armour/Armour.cs
@@ -0,0 +1,20 @@
+﻿// Author: Joon Song, Steven Ung
+// File Name: Armour.cs
+// Project Name: ISU_Medieval_Odyssey
+// Creation Date: 12/18/2018
+// Modified Date: 12/18/2018
+// Description: Class to hold Armour object
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISU_Medieval_Odyssey
+{
+    public abstract class Armour : Item
+    {
+
+    }
+}
---
+++ ISU_Medieval_Odyssey/Item/Item.cs
@@ -0,0 +1,19 @@
+﻿// Author: Joon Song, Steven Ung
+// File Name: Item.cs
+// Project Name: ISU_Medieval_Odyssey
+// Creation Date: 12/18/2018
+// Modified Date: 12/18/2018
+// Description: Class to hold Item object
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISU_Medieval_Odyssey
+{
+    public abstract class Item
+    {
+    }
+}
