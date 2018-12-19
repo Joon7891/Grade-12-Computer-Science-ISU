4f8009f63b75e9c9bbe858dd361f0cdafa4b77bb
Joon Song <joon7891@gmail.com>
2018-12-19 14:04:29 -0500

    Test commit

---
+++ ISU_Medieval_Odyssey/Player/Player.cs
@@ -0,0 +1,44 @@
+﻿// Author: Joon Song
+// File Name: Player.cs
+// Project Name: ISU_Medieval_Odyssey
+// Creation Date: 12/19/2018
+// Modified Date: 12/19/2018
+// Description: Class to hold Player object
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ISU_Medieval_Odyssey
+{
+    public sealed class Player
+    {
+        // Instances of each type of armour
+        private Shoes shoes;
+        private Belt belt;
+
+        static Player()
+        {
+
+        }
+
+        public Player()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
