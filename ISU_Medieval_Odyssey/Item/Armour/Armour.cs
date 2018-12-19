00ad78164172f8ec8205d03c63a387da062106f0
Joon Song <joon7891@gmail.com>
2018-12-19 12:02:36 -0500

    Added Armour draw method

--- ISU_Medieval_Odyssey/Item/Armour/Armour.cs
+++ ISU_Medieval_Odyssey/Item/Armour/Armour.cs
@@ -10,12 +10,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace ISU_Medieval_Odyssey
 {
     public abstract class Armour : Item
     {
+        // Dictionary to map MovementTypes to the appropriate images
         protected Dictionary<MovementType, Texture2D[,]> movementImages = new Dictionary<MovementType, Texture2D[,]>();
 
         /// <summary>
@@ -34,15 +36,29 @@ namespace ISU_Medieval_Odyssey
             // Loading in images for each direction and frame
             for (Direction direction = Direction.Up; direction <= Direction.Left; ++direction)
             {
-                for (int i = 0; i < numFrames; ++i)
+                for (byte i = 0; i < numFrames; ++i)
                 {
-                    loadedImages[(int)direction, i] = Main.Content.Load<Texture2D>
+                    loadedImages[(byte)direction, i] = Main.Content.Load<Texture2D>
                         (basePath + $"{movementType.ToString()}/{direction.ToString()}/{armourName}{movementType.ToString()}{direction.ToString()}{i}");
                 }
             }
 
             // Returning loaded images
             return loadedImages;
+        }
+
+        /// <summary>
+        /// Draw subprogram for Armour object
+        /// </summary>
+        /// <param name="spriteBatch">SpriteBatch to draw sprites</param>
+        /// <param name="playerRectangle">The corresponding player's rectangle</param>
+        /// <param name="movementType">The movement type</param>
+        /// <param name="direction">The current direction</param>
+        /// <param name="frameNo">The current frame number</param>
+        public void Draw(SpriteBatch spriteBatch, Rectangle playerRectangle, MovementType movementType, Direction direction, byte frameNo)
+        {
+            // Drawing armour
+            spriteBatch.Draw(movementImages[movementType][(byte)direction, frameNo] , playerRectangle, Color.White);
         }
     }
 }
