91ab23a96df5a7cf6a84486a899d56a7a8507a3c
Joon Song <joon7891@gmail.com>
2018-12-20 00:14:56 -0500

    Added various weapons classes

---
+++ ISU_Medieval_Odyssey/Item/Weapon/SlashWeapon/Sword.cs
@@ -0,0 +1,20 @@
+﻿// Author: Joon Song
+// File Name: Sword.cs
+// Project Name: ISU_Medieval_Odyssey
+// Creation Date: 12/19/2018
+// Modified Date: 12/19/2018
+// Description: Class to hold Sword object
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ISU_Medieval_Odyssey
+{
+    public sealed class Sword : SlashWeapon
+    {
+    }
+}
