c340db2d651d657e2b1a51d1d0ed8eec9ced7d7c
Joon Song <joon7891@gmail.com>
2018-12-27 15:01:48 -0500

    Added Hair graphics

---
+++ ISU_Medieval_Odyssey/Items/Armour/Helmet/Hair.cs
@@ -0,0 +1,29 @@
+﻿// Author: Joon Song
+// File Name: Hair.cs
+// Project Name: ISU_Medieval_Odyssey
+// Creation Date: 12/27/2018
+// Modified Date: 12/27/2018
+// Description: Class to hold Hair object - default for no helmet
+
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ISU_Medieval_Odyssey
+{
+    public sealed class Hair : Helmet
+    {
+        // Dictionary to map MovementTypes to the appropriate images
+        private new static Dictionary<MovementType, Texture2D[,]> movementImages = new Dictionary<MovementType, Texture2D[,]>();
+
+        /// <summary>
+        /// Static constructor to setup various <see cref="Hair"/> components
+        /// </summary>
+        static Hair()
+        {
+            // Temporary strings to help with file paths
+            string basePath = "Images/Sprites/Armour/Helmet/Hair/";
+            string armourTypeName = "hair";
+            movementImages = EntityHelper.LoadMovementImages(basePath, armourTypeName);
+        }
+    }
+}
