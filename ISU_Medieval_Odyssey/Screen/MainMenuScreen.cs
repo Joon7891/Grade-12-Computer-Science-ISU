ad157b980c93fbc3664c1db8df380956da6b732b
Joon Song <joon7891@gmail.com>
2019-01-03 23:57:13 -0500

    Added main menu game title

--- ISU_Medieval_Odyssey/Screen/MainMenuScreen.cs
+++ ISU_Medieval_Odyssey/Screen/MainMenuScreen.cs
@@ -16,6 +16,9 @@ namespace ISU_Medieval_Odyssey
         // Various main menu screen components
         private Song backgroundMusic;
         private Background background;
+        private SpriteFont titleFont;
+        private string[] titleText = { "Medieval", "Odyssey" };
+        private Vector2[] titleLocation = new Vector2[2];
         private Button[] optionButtons = new Button[3];
         
         /// <summary>
@@ -26,6 +29,11 @@ namespace ISU_Medieval_Odyssey
             // Setting up background components
             backgroundMusic = Main.Content.Load<Song>("Audio/Music/mainMenuBackgroundMusic");
             background = new Background(Main.Content.Load<Texture2D>("Images/Backgrounds/mainMenuBackgroundImage"));
+            titleFont = Main.Content.Load<SpriteFont>("Fonts/TitleFont");
+            for (byte i = 0; i < titleLocation.Length; ++i)
+            {
+                titleLocation[i] = new Vector2((SharedData.SCREEN_WIDTH - titleFont.MeasureString(titleText[i]).X) / 2, 30 + 150 * i);
+            }
 
             // Setting up option buttons
             optionButtons[0] = new Button(Main.Content.Load<Texture2D>("Images/Sprites/Buttons/newGameButton"), new Rectangle(SharedData.SCREEN_WIDTH / 2 - 150, 400, 300, 100),() =>
@@ -75,6 +83,12 @@ namespace ISU_Medieval_Odyssey
             
             // Drawing background
             background.Draw(spriteBatch);
+
+            // Drawing game title
+            for (int i = 0; i < titleText.Length; ++i)
+            {
+                spriteBatch.DrawString(titleFont, titleText[i], titleLocation[i], Color.SaddleBrown * 0.8f);
+            }
 
             // Drawing various main menu buttons
             for (int i = 0; i < optionButtons.Length; ++i)
